7277e4580b9101708eac35403ef59380ed0caea7
ClementSellyPro <clementselly.pro@gmail.com>
2025-04-24 13:16:46 +0400

    Add new books setup + multer-config

--- backend-livres/controllers/books.js
+++ backend-livres/controllers/books.js
@@ -1,10 +1,21 @@
 const Book = require('../models/Book');
 
 exports.createBook = (req, res, next) => {
-  delete req.body._id;
+  console.log("This is the req.body.book ::: ", req.body.book);
+  const bookObject = JSON.parse(req.body.book);
+  delete bookObject._id;
+  delete bookObject._userID;
+
+  console.log("This is the req.body.book after delete id ::: ", bookObject);
+
   const book = new Book({
-    ...req.body
+    ...bookObject,
+    userID: req.auth.userID,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
+  delete book._id;
+  console.log("This is the book::: ", book)
   book.save()
   .then(() => res.status(201).json({ message: "Livre correctement ajouté" }))
+  .catch(error => res.status(400).json({ error: "Erreur ::: ", error }))
 }
--- backend-livres/routes/books.js
+++ backend-livres/routes/books.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
 const router = express.Router();
 const booksCtrl = require('../controllers/books');
 
-router.post('/', booksCtrl.createBook);
+router.post('/', auth, multer, booksCtrl.createBook);
 
 module.exports = router;
